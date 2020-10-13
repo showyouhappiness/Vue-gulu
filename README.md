d40e006cab17a078689cd58cb9307d277f930213
liyongjun <1477676296@qq.com>
2020-10-13 20:00:07 +0800

    完成一个按钮的基本样式

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 这是一个Vue ui 库
 作者： 李勇军
+## 安装
+使用本框架前，请在css中开启border-box
+、、、
+box-sizing: border-box;
+、、、
---
+++ button.js
@@ -0,0 +1,7 @@
+const { component } = require("vue/types/umd")
+
+Vue.component(g - button, {
+    template: `
+        <button class="g-button">按钮</button>
+    `
+})
