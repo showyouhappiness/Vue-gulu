50f07d60563ff192b4bce0161640feaa78538bf3
liyongjun <1477676296@qq.com>
2020-11-10 20:15:50 +0800

    完成第一个按钮 提交

--- button.js
+++
@@ -1,7 +0,0 @@
-const { component } = require("vue/types/umd")
-
-Vue.component(g - button, {
-    template: `
-        <button class="g-button">按钮</button>
-    `
-})
---
+++ src/button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+.g-button {
+  height: var(--button-height);
+  font-size: var(--font-size);
+  padding: 0 1em;
+  background: var(--button-bg);
+  border: 1px solid var(--border-color);
+  border-radius: var(--border-radius);
+
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+</style>
