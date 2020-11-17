6240764e45b0717669db103f44dcb2d103eba5a9
liyongjun <1477676296@qq.com>
2020-11-17 21:56:18 +0800

    更新button 新增加载中icon

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
   <button class="g-button" :class="{ [`icon-${iconPosition}`]: true }">
     <g-icon :name="icon" class="icon" v-if="icon"></g-icon>
+    <g-icon name="loading" class="loading"></g-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -23,6 +24,15 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+
+        100% {
+            transform: rotate(360deg);
+        }
+    }
 .g-button {
   height: var(--button-height);
   font-size: var(--font-size);
@@ -66,5 +76,8 @@ export default {
       order: 1;
     }
   }
+    .loading {
+        animation: spin 2s infinite linear;
+    }
 }
 </style>
