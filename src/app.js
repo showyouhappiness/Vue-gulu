82fb9c6136f583d5ae62d3ede7ac84db475f4a88
liyongjun <1477676296@qq.com>
2020-11-23 22:43:07 +0800

    部分单元测试

--- src/app.js
+++ src/app.js
@@ -7,9 +7,27 @@ Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
 new Vue({
     el: "#app",
-    data:{
+    data: {
         loading1: false,
         loading2: true,
         loading3: false,
     }
 })
+
+
+//单元测试
+import chai from 'chai'
+const expect = chai.expect
+
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    button.$mount('#test')
+    let useElement = button.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-settings')
+}
