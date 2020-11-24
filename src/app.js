f8d5f0090773c77e33ff7cbb05d03098a80435e7
liyongjun <1477676296@qq.com>
2020-11-24 21:17:58 +0800

    使用chai.spy 监听回调函数

--- src/app.js
+++ src/app.js
@@ -17,7 +17,9 @@ new Vue({
 
 //单元测试
 import chai from 'chai';
-const expect = chai.expect; 
+import spies from 'chai-spies';
+chai.use(spies)
+const expect = chai.expect
 {
     const Constructor = Vue.extend(Button)
     const vm = new Constructor({
@@ -32,7 +34,7 @@ const expect = chai.expect;
     vm.$el.remove()
     vm.$destroy()
 }
- {
+{
     const Constructor = Vue.extend(Button)
     const vm = new Constructor({
         propsData: {
@@ -46,7 +48,7 @@ const expect = chai.expect;
     expect(href).to.eq('#icon-loading')
     vm.$el.remove()
     vm.$destroy()
-} 
+}
 {
     const div = document.createElement('div')
     document.body.appendChild(div)
@@ -58,7 +60,7 @@ const expect = chai.expect;
     })
     vm.$mount(div)
     let svg = vm.$el.querySelector('svg')
-    let {order} = window.getComputedStyle(svg)
+    let { order } = window.getComputedStyle(svg)
     expect(order).to.eq('1')
     vm.$el.remove()
     vm.$destroy()
@@ -75,22 +77,23 @@ const expect = chai.expect;
     })
     vm.$mount(div)
     let svg = vm.$el.querySelector('svg')
-    let {order} = window.getComputedStyle(svg)
+    let { order } = window.getComputedStyle(svg)
     expect(order).to.eq('2')
     vm.$el.remove()
     vm.$destroy()
 }
 {
+    //mock
     const Constructor = Vue.extend(Button)
-    const gButton = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'settings',
         }
     })
-    gButton.$mount()
-    gButton.$on('click',function(){
-        expect(1).to.eq(1)
-    })
-    let vm = gButton.$el
-    vm.click()
+    vm.$mount()
+    let spy = chai.spy(function(){})
+    vm.$on('click', spy)
+    let button = vm.$el
+    button.click()
+    expect(spy).to.have.been.called()
 }
