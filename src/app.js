28ae134f9af57db1d05eeacb297f4acf66c7595d
liyongjun <1477676296@qq.com>
2020-11-24 19:39:21 +0800

    单元测试

--- src/app.js
+++ src/app.js
@@ -12,13 +12,12 @@ new Vue({
         loading2: true,
         loading3: false,
     }
-})
+});
 
 
 //单元测试
-import chai from 'chai'
-const expect = chai.expect
-
+import chai from 'chai';
+const expect = chai.expect; 
 {
     const Constructor = Vue.extend(Button)
     const button = new Constructor({
@@ -29,5 +28,69 @@ const expect = chai.expect
     button.$mount('#test')
     let useElement = button.$el.querySelector('use')
     let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-settings')
+    expect(href).to.eq('#icon-settings')
+    button.$el.remove()
+    button.$destroy()
+}
+ {
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading: true
+        }
+    })
+    button.$mount()
+    let useElement = button.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-loading')
+    button.$el.remove()
+    button.$destroy()
+} 
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const gButton = new Constructor({
+        propsData: {
+            icon: 'settings',
+        }
+    })
+    gButton.$mount()
+    gButton.$on('click',function(){
+        expect(1).to.eq(1)
+    })
+    let button = gButton.$el
+    button.click()
 }
