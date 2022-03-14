0b4cf6142397b638b8de497f78ef98ab67776761
liyongjun <1477676296@qq.com>
2022-03-14 22:58:12 +0800

    col和row的完善与重构

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col" :class="colClass" :style="colStyle">
+    <div style="border: 1px solid green; height: 100px;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -10,7 +12,32 @@ export default {
   props:{
     span:{
       type:[Number, String]
+    },
+    offset:{
+      type:[Number,String]
     }
+  },
+  data(){
+    return{
+      gutter:0
+    }
+  },
+  computed:{
+    colClass(){
+      let {span, offset} = this
+      // xxx && `xxx-${xxx}` 如果存在则使用后面的，如果不存在则无
+      return [span && `col-${span}`, offset && `offset-${offset}`]
+    },
+    colStyle(){
+      return{
+          paddingLeft: this.gutter / 2+'px',
+          paddingRight: this.gutter / 2+'px'
+      }
+    }
+  },
+  created() {
+  },
+  mounted() {
   }
 }
 </script>
@@ -18,14 +45,20 @@ export default {
 <style scoped lang="scss">
   .col {
     height: 100px;
-    background: grey;
     width: 50%;
-    border: 1px solid red;
+    padding: 0 10px;
 
     $class-prefix: col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n}{
         width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-;
+    @for $m from 1 through 24 {
+      &.#{$class-prefix}#{$m}{
+        margin-left: ($m / 24) * 100%;
       }
     }
   }
--- src/row.vue
+++ src/row.vue
@@ -1,12 +1,30 @@
 <template>
-<div class="row">
+<div class="row" :style="rowStyle">
   <slot></slot>
 </div>
 </template>
 
 <script>
 export default {
-  name: "row"
+  name: "GuluRow",
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  computed:{
+    rowStyle() {
+      return {
+        marginLeft: -this.gutter / 2 + 'px',
+        marginRight: -this.gutter / 2 + 'px'
+      }
+    }
+  },
+  mounted() {
+    this.$children.forEach((vm)=>{
+      vm.gutter = this.gutter
+    })
+  }
 }
 </script>
 
