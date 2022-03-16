36610775ad0ef7720789ee3ff1f8107d2b0ef90b
liyongjun <1477676296@qq.com>
2022-03-16 12:44:04 +0800

    稍微重构了一下

--- src/col.vue
+++ src/col.vue
@@ -1,37 +1,70 @@
 <template>
   <div class="col" :class="colClass" :style="colStyle">
-    <div style="border: 1px solid green;">
-      <slot></slot>
-    </div>
+    <slot></slot>
   </div>
 </template>
 
 <script>
+let validator = (value) => {
+  let keys = Object.keys(value)
+  let valid = true
+  keys.forEach(key => {
+    if (!['span', 'offset'].includes(key)) {
+      valid = false
+    }
+  })
+  return valid
+}
 export default {
   name: "GuluCol",
-  props:{
-    span:{
-      type:[Number, String]
+  props: {
+    span: {
+      type: [Number, String]
     },
-    offset:{
-      type:[Number,String]
+    offset: {
+      type: [Number, String]
+    },
+    ipad: {type: Object, validator},
+    narrow: {type: Object, validator},
+    pc: {type: Object, validator},
+    wide: {type: Object, validator},
+  },
+  data() {
+    return {
+      gutter: 0
     }
   },
-  data(){
-    return{
-      gutter:0
+  methods:{
+    createClasses (obj, str = ''){
+      if (!obj) return []
+      let array = []
+      if (obj.span) {
+        array.push(`col-${str}${obj.span}`)
+      }
+      if (obj.offset) {
+        array.push(`offset-${str}${obj.offset}`)
+      }
+      return array
     }
   },
-  computed:{
-    colClass(){
-      let {span, offset} = this
+  computed: {
+    colClass() {
+      let {span, offset, ipad, narrow, pc, wide} = this
       // xxx && `xxx-${xxx}` 如果存在则使用后面的，如果不存在则无
-      return [span && `col-${span}`, offset && `offset-${offset}`]
+      let createClasses = this.createClasses
+
+      return [
+        ...createClasses({span, offset}),
+        ...createClasses(ipad, [`ipad-`]),
+        ...createClasses(narrow, [`narrow-`]),
+        ...createClasses(pc, [`pc-`]),
+        ...createClasses(wide, [`wide-`]),
+      ]
     },
-    colStyle(){
-      return{
-          paddingLeft: this.gutter / 2+'px',
-          paddingRight: this.gutter / 2+'px'
+    colStyle() {
+      return {
+        paddingLeft: this.gutter / 2 + 'px',
+        paddingRight: this.gutter / 2 + 'px'
       }
     }
   },
@@ -43,19 +76,83 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  .col {
-    $class-prefix: col-;
+.col {
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%;
+    }
+  }
+
+  $class-prefix: offset-;
+  @for $m from 1 through 24 {
+    &.#{$class-prefix}#{$m} {
+      margin-left: ($m / 24) * 100%;
+    }
+  }
+
+  @media (min-width: 577px) {
+    $class-prefix: col-ipad-;
     @for $n from 1 through 24 {
-      &.#{$class-prefix}#{$n}{
+      &.#{$class-prefix}#{$n} {
         width: ($n / 24) * 100%;
       }
     }
 
-    $class-prefix: offset-;
+    $class-prefix: offset-ipad-;
     @for $m from 1 through 24 {
-      &.#{$class-prefix}#{$m}{
+      &.#{$class-prefix}#{$m} {
         margin-left: ($m / 24) * 100%;
       }
     }
   }
+
+  @media (min-width: 769px) {
+    $class-prefix: col-narrow-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-narrow-;
+    @for $m from 1 through 24 {
+      &.#{$class-prefix}#{$m} {
+        margin-left: ($m / 24) * 100%;
+      }
+    }
+  }
+
+  @media (min-width: 993px) {
+    $class-prefix: col-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-pc-;
+    @for $m from 1 through 24 {
+      &.#{$class-prefix}#{$m} {
+        margin-left: ($m / 24) * 100%;
+      }
+    }
+  }
+
+  @media (min-width: 1200px) {
+    $class-prefix: col-wide-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-wide-;
+    @for $m from 1 through 24 {
+      &.#{$class-prefix}#{$m} {
+        margin-left: ($m / 24) * 100%;
+      }
+    }
+  }
+}
 </style>
