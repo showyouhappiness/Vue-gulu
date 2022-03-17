9908206f21abdda9de8524585e7053143bf35978
liyongjun <1477676296@qq.com>
2022-03-17 18:48:47 +0800

    layout header content sider footer 完成

---
+++ src/content.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="content">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "Gulucontent"
+}
+</script>
+
+<style scoped lang="scss">
+.content {
+  flex-grow: 1;
+}
+</style>
---
+++ src/header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Guluheader"
+}
+</script>
+
+<style scoped lang="scss">
+.header {
+}
+</style>
