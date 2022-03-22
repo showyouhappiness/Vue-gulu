f615f931a6b769dd07e581d54d4cd1a82fff3744
liyongjun <1477676296@qq.com>
2022-03-22 20:29:56 +0800

    实现实现了只能有一个toast的功能

--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,23 @@
 import Toast from "./toast";
 
+function createToast({Vue, message, propsData}) {
+    let Constructor = Vue.extend(Toast)
+    let toast = new Constructor({propsData})
+    toast.$slots.default = [message] // 把数据赋值给插槽的默认值
+    toast.$mount()
+    document.body.appendChild(toast.$el)  // 把元素放在body里
+    return toast
+}
+
+let currentToast
 export default {
     install(Vue, options) {
         Vue.prototype.$toast = function (message, toastOptions) {
-            let Constructor = Vue.extend(Toast)
-            let toast = new Constructor({
-                propsData: toastOptions
-            })
-            toast.$slots.default = [message] // 把数据赋值给插槽的默认值
-            toast.$mount()
-            document.body.appendChild(toast.$el)  // 把元素放在body里
+            if (currentToast) {
+                console.log('haha')
+                currentToast.close()
+            }
+            currentToast = createToast({Vue, message, propsData: toastOptions})
         }
     }
 }
