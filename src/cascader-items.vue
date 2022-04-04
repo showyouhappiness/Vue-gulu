f441d5cac63cc25e139e3e0151fb3e0b648c39fd
liyongjun <1477676296@qq.com>
2022-04-04 11:47:18 +0800

    确定渲染内容

---
+++ src/cascader-items.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="cascaderItem">
+    <div class="left">
+      <div class="label" v-for="item in items" @click="leftSelected = item">
+        {{ item.name }}
+      </div>
+    </div>
+    <div class="right" v-if="rightItems">
+      <gulu-cascader-items :items="rightItems"></gulu-cascader-items>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GuluCascaderItems",  // 必须与组件名称一致, 否则编译会报错 gulu-cascader-item -> GuluCascaderItem
+  props: {
+    items: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      leftSelected: null,
+    }
+  },
+  computed: {
+    rightItems() {
+      if (this.leftSelected && this.leftSelected.children) {
+        return this.leftSelected.children
+      } else {
+        return null
+      }
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import "var";
+
+.cascaderItem {
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+
+  .right {
+    border-left: 1px solid $border-color;
+  }
+}
+
+
+</style>
