7fa062646d33f0367234c19e7a8b3641a6789d33
liyongjun <1477676296@qq.com>
2022-04-06 19:05:08 +0800

    完成轮播图组件

--- src/slides-item.vue
+++ src/slides-item.vue
@@ -1,68 +1,69 @@
 <template>
-  <transition name="slide">
-    <div class="g-slides-item" v-if="visible" :class="{reverse}">
-      <slot></slot>
-    </div>
-  </transition>
+  <div>
+    <template v-if="animationEnabled">
+      <transition name="slide">
+        <div class="g-slides-item" v-if="visible" :class="{reverse}">
+          <slot></slot>
+        </div>
+      </transition>
+    </template>
+    <template v-else>
+      <div class="g-slides-item" v-if="visible" :class="{reverse}">
+        <slot></slot>
+      </div>
+    </template>
+  </div>
 </template>
 
 <script>
-export default {
-  name: "GuluSlidesItem",
-  props: {
-    name: {
-      type: String,
-      required: true
+  export default {
+    name: 'GuluSlidesItem',
+    props: {
+      name: {
+        type: String,
+        required: true
+      }
+    },
+    data () {
+      return {
+        selected: undefined,
+        reverse: false,
+        animationEnabled: false
+      }
+    },
+    updated () {
+      this.animationEnabled = true
+    },
+    computed: {
+      visible () {
+        return this.selected === this.name
+      }
     }
-  },
-  data() {
-    return {
-      selected: undefined,
-      reverse: false
-    }
-  },
-  computed: {
-    visible() {
-      return this.selected === this.name;
-    }
-  },
-}
+  }
 </script>
 
-<style scoped lang="scss">
-.g-slides-item {
-
-}
-
-.slide-leave-active {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-
-.slide-leave-active, .slide-enter-active {
-  transition: all .3s ease-in-out;
-}
-
-.slide-enter {
-  transform: translateX(100%);
-  opacity: 0;
-}
-
-.slide-enter.reverse {
-  transform: translateX(-100%);
-  opacity: 0;
-}
-
-.slide-leave-to {
-  transform: translateX(-100%) scale(0.8);
-  opacity: 0;
-}
-
-.slide-leave-to.reverse {
-  transform: translateX(100%) scale(0.8);
-  opacity: 0;
-}
+<style lang="scss" scoped>
+  .g-slides-item {
+  }
+  .slide-leave-active {
+    position: absolute;
+    left: 0; top: 0;
+    width: 100%;
+    height: 100%;
+  }
+  .slide-enter-active, .slide-leave-active {
+    transition: all 0.5s;
+  }
+  .slide-enter {
+    transform: translateX(100%);
+  }
+  .slide-enter.reverse {
+    transform: translateX(-100%);
+  }
+  .slide-leave-to {
+    transform: translateX(-100%);
+  }
+  .slide-leave-to.reverse {
+    transform: translateX(100%);
+  }
 </style>
